cdfa6c4b2159b7828c07254e477e85d3b4fbfd09
Ranel John Tormis <raneljohnt@gmail.com>
2024-07-26 21:28:44 +0800

    feat: Added passport JWT and social

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,14 @@
 import express from "express";
-import app, { PORT } from "@utilities/general";
+import app, {
+  FACEBOOK_APP_ID,
+  FACEBOOK_APP_SECRET,
+  PORT,
+  YELPCAMP_APP_SECRET_KEY,
+} from "@utilities/general";
+import morgan from "morgan";
+import passport from "passport";
+import passportFacebook from "passport-facebook";
+import { ExtractJwt, Strategy } from "passport-jwt";
 
 /**
  * Routes
@@ -15,8 +24,47 @@ import { connectMoongoseDB } from "@utilities/database";
  */
 connectMoongoseDB();
 
+/**
+ * PASSPORT
+ */
+const FacebookStrategy = passportFacebook.Strategy;
+
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: YELPCAMP_APP_SECRET_KEY,
+};
+
 app.use(express.json());
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: FACEBOOK_APP_ID,
+      clientSecret: FACEBOOK_APP_SECRET,
+      callbackURL: "http://localhost:3000/auth/facebook/callback",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      // TODO: Implement Facebook strat
+      // User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      //   return cb(err, user);
+      // });
+    }
+  )
+);
+
+passport.use(
+  new Strategy(opts, async (payload, done) => {
+    // TODO:Implement JWT strat
+    // try {
+    //   const user = UserModel.findById(payload.id);
+    //   if (user) return done(null, user);
+    // } catch (error) {
+    //   return done(error);
+    // }
+  })
+);
 
 app.use("/users", userRoute);
 app.use("/campgrounds", campgroundRoute);
--- backend/src/utilities/general.ts
+++ backend/src/utilities/general.ts
@@ -5,6 +5,10 @@ dotenv.config();
 // Backend port
 export const PORT: number = parseInt(process.env.PORT as string);
 export const DATABASE: string = process.env.DATABASE as string;
+export const FACEBOOK_APP_SECRET: string = process.env.FACEBOOK_APP_SECRET as string;
+export const FACEBOOK_APP_ID: string = process.env.FACEBOOK_APP_ID as string;
+export const YELPCAMP_APP_SECRET_KEY: string = process.env.YELPCAMP_APP_SECRET_KEY as string;
+
 const app = express();
 
 /**
